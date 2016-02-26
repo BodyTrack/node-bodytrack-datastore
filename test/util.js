186d27d72b78ac6ecf3cf6effa97d651d32f6df5
Chris Bartley <bartley@cmu.edu>
2016-02-26 10:53:02 -0500

    Added isPositiveInt() util method

--- test/util.js
+++ test/util.js
@@ -166,3 +166,111 @@ describe("isInt()", function() {
       util.isInt(-1.000000000000000000001).should.be.true();
    });
 });
+
+describe("isPositiveInt()", function() {
+   var doTest = function(i, expectedOutcome) {
+      var msg = null;
+      if (util.isString(i)) {
+         msg = 'should return ' + expectedOutcome + ' for "' + i + '"';
+      }
+      else {
+         msg = 'should return ' + expectedOutcome + ' for ' + i;
+      }
+
+      expectedOutcome = !!expectedOutcome;
+      it(msg, function(done) {
+         if (expectedOutcome) {
+            util.isPositiveInt(i).should.be.true();
+         }
+         else {
+            util.isPositiveInt(i).should.be.false();
+         }
+         done();
+      });
+   };
+
+   var doTestForNumAndString = function(num, expectedOutcome) {
+      doTest(num, expectedOutcome);
+      doTest(String(num), expectedOutcome);
+   };
+
+   for (var i = 1; i < 20; i++) {
+      doTestForNumAndString(i, true);
+   }
+   doTestForNumAndString(42, true);
+   doTestForNumAndString(343, true);
+   doTestForNumAndString(101010, true);
+   it("should return true for 42.000000000000000001", function(done) {
+      util.isPositiveInt(42.000000000000000001).should.be.true();
+      done();
+   });
+
+   doTestForNumAndString(0, false);
+   // TODO: doTest(01, false);
+   doTestForNumAndString(-1, false);
+   doTestForNumAndString(-10, false);
+   doTestForNumAndString(-10.0, false);
+   doTestForNumAndString(-10.3, false);
+   doTest('42.', false);
+   doTest('42.0', false);
+   doTest('42.1', false);
+   doTest('42.000000000000000001', false);
+   doTest('', false);
+   doTest(null, false);
+
+   var x;
+   doTest(x, false);
+   doTest(1 / 0, false);
+   doTest(-1 / 0, false);
+   doTest(parseInt("bogus"), false);
+
+   it("should return true for .000000000000000001", function(done) {
+      util.isPositiveInt(.000000000000000001).should.be.false();
+      done();
+   });
+   it("should return false for 'bogus'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for '0123'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for '123a'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for 'a123'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for '1a23'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for ' 1'", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for '1 '", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for an array", function(done) {
+      util.isPositiveInt([]).should.be.false();
+      done();
+   });
+
+   it("should return false for an object", function(done) {
+      util.isPositiveInt({}).should.be.false();
+      done();
+   });
+
+});   // End isPositiveInt()
